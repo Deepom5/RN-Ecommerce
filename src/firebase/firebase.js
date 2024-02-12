269701a600623c2fb9d9de09c8aaf8c16cd3467a
Deepom5 <64247104+Deepom5@users.noreply.github.com>
2024-02-12 11:34:46 +0530

    Update

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
 export const baseUrl = "https://ecommerce-react-native-course-default-rtdb.firebaseio.com/"
-export const apiKey = "AIzaSyDARhLU_6eCfR8i-nUKcZLKPzjmRZXEbpA"
-export const authUrl = "https://identitytoolkit.googleapis.com/v1/"
+export const apiKey = ""
+export const authUrl = ""
 
 
